ca2c579720d80b7d288d9b0ef9315e837f28495c
rjsmart021 <64855625+rjsmart021@users.noreply.github.com>
2024-04-30 21:56:05 -0500

    Add files via upload

---
+++ 4.Lesson 1: Assignments ] Modules.py
@@ -0,0 +1,20 @@
+#Lesson 1: Assignments ] Modules
+#1.Python Modules and Data HanddlingAssignment
+#Task 1: Your Mood Today
+# moody.py
+def mood_response(mood):
+    mood = input("How are you feeling today? ")
+    if mood == "Happy":
+        print("Smile, its a beautiful day!")
+    elif mood == "Sad":
+        print("Cheer up buddy!, you are loved!")
+    elif mood == "Okay":
+        print("Be happy, Yay!")
+    else:
+        print("That's a great Choice")
+    # main.py
+    import mood_response(mood):
+    mood = input("How are you feeling today? ")
+    mood_response(mood)
+    mood = input("How are you feeling today? ")
+    print(mood_responses.mood_response(mood))
